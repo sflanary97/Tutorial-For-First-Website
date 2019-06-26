960ab05d1e7ea54c7e5920a922313944b9bdb711
sflanary97 <sflanary97@gmail.com>
2019-06-26 16:33:06 -0500

    Updates

---
+++ precache-manifest.39034d8885c59fa278222f17c406b7a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea30b02fde1fa36c0e5cb3d55ca821c1",
+    "url": "/Tutorial-For-First-Website/index.html"
+  },
+  {
+    "revision": "81cb124c9dda71f85e17",
+    "url": "/Tutorial-For-First-Website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fc42e8640783da91735a",
+    "url": "/Tutorial-For-First-Website/static/js/2.6133a7e1.chunk.js"
+  },
+  {
+    "revision": "81cb124c9dda71f85e17",
+    "url": "/Tutorial-For-First-Website/static/js/main.b6814c39.chunk.js"
+  },
+  {
+    "revision": "f378f6730aae526cdddc",
+    "url": "/Tutorial-For-First-Website/static/js/runtime~main.2a3f5b25.js"
+  }
+]);

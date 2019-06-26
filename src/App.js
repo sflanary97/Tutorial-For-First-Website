d0c70c3396094e9df87f48447281eadb0db3acb8
sflanary97 <sflanary97@gmail.com>
2019-06-26 16:37:11 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography>
+              My Website
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Card>
+          <CardContent>
+            <Typography>
+              Hello, here's some content about my website
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
     </div>
   );
 }
